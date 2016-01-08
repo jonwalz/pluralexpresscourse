93e990c60bf54705c05ad99454f97c27e34f1330
Jon Walz <jon.walz.music@gmail.com>
2016-01-08 07:48:39 -0800

    Changed view to jsx

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var nodemon = require('gulp-nodemon');
+var inject = require('gulp-inject');
+
+var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {
+    read: false
+});
+
+
 
 var jsFiles = ['*.js', 'src/**/*.js'];
+
+// ---- Tasks ---- //
 
 gulp.task('style', function () {
     return gulp.src(jsFiles)
@@ -16,13 +25,21 @@ gulp.task('style', function () {
 
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../public'
     };
 
-    return gulp.src('./src/views/*.html')
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+    var injectOptions = {
+        ignorePath: "/public"
+    }
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
 
@@ -32,7 +49,6 @@ gulp.task('serve', ['style', 'inject'], function () {
         delayTime: 1,
         env: {
             'PORT': 3000
-
         },
         watch: jsFiles
     };
--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,12 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', 'src/views');
-app.set('view engine', 'jade');
+
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('Hello Hello');
+    res.render('index', {title: "This is the title", list: ["a", "b"]});
 });
 
 app.listen(port, function (err) {

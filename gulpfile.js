5e1a391f4715f82ec13acad4d71480da5743e1ed
Jon Walz <jon.walz.music@gmail.com>
2016-01-23 20:52:25 -0800

    Added the beginnings of user auth

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,10 @@ var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
     return gulp.src(jsFiles)
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', {
-            verbose: true
-        }))
+        //.pipe(jshint())
+        //.pipe(jshint.reporter('jshint-stylish', {
+        //    verbose: true
+        //}))
         .pipe(jscs());
 });
 

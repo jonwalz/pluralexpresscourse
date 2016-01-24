e590b07737da95eceb404e40aae175ff1df2f6fb
Jon Walz <jon.walz.music@gmail.com>
2016-01-23 21:37:50 -0800

    Added user and pass functionality to database

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,13 +6,26 @@ var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log(req.body);
-            req.login(req.body, function () {
-                res.redirect('/auth/profile')
-            })
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                var user = {
+                    username: req.body.userName,
+                    password: req.body.password
+                }
+
+                collection.insert(user, function (err, results) {
+                    req.login(results.ops[0], function () {
+                        res.redirect('/auth/profile')
+                    })
+                });
+            });
+
         });
     authRouter.route('/profile')
-        .get(function(req, res) {
-           res.json(req.user);
+        .get(function (req, res) {
+            res.json(req.user);
         });
     return authRouter;
 };
